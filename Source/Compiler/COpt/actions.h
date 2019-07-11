09d54c51367eccec000a0294b91acf1ae28f03d0
Jeff Teunissen <deek@d2dc.net>
2019-07-10 23:18:09 -0400

    copt: Add some optimization rules
    Let's add some rules, most of which are exposed by looking at the
    difference in the output between c.opt and the "t.opt" we're working with,
    and some others based on my understanding of the unknown-origin table below
    in the source.
    
    These chained rules might seem like they're backwards, (because they look
    like they're forwards, but are hooked up in reverse) but they're not --
    the instructions are matched backwards and then replaced backwards again, so
    it all works out correctly in the end.
    
    The changes reduce the differences in output significantly, but for some
    reason it doesn't work if the one of the lines contains a label.
    
    New optimizations:
    
    sex or clra, followed by cmpd #num, will become cmpb #num if, and only if,
    #num is less than 256.
    
    leas 2,s followed by pshs d becomes std ,s
    leas 2,s followed by std -2,s becomes std ,s++
    leau 1,u followed by ldb -1,u becomes ldb ,u+
    
    sex followed by clra becomes clra
    
    pshs d followed by pshs x becomes pshs d,x
    pshs d followed by pshs y becomes pshs d,y
    
    pshs u followed by leas -2,s becomes pshs d,u
    pshs u followed by leas -4,s becomes pshs d,x,u
    pshs u followed by leas -6,s becomes pshs d,x,y,u
    
    Unfortunately, since the pshs u stuff really only happens at the beginning
    of a function, and since labels cause optimizations to not happen, they
    don't appear to do anything. But once I can get the optimizer to move
    labels forward (never backward, that could cause a branch to land in a
    place it's not supposed to), that should enable them to work.

--- Source/Compiler/COpt/actions.h
+++ Source/Compiler/COpt/actions.h
@@ -43,7 +43,34 @@ static instr
     act3a = {&act3b,    pshs,   d},     rep3a = {NULL,      std,    zs},
 
     act4b = {NULL,      leas,   p2s},
-    act4a = {&act4b,    std,    m2s},   rep4a = {NULL,      std,    ",s++"};
+    act4a = {&act4b,    std,    m2s},   rep4a = {NULL,      std,    ",s++"},
+
+    act5b = {NULL,      leau,   p1u},
+    act5a = {&act5b,    ldb,    m1u},   rep5a = {NULL,      NULL,   ",u+"},
+
+    act6b = {NULL,      sex,    NULL},
+    act6a = {&act6b,    cmpd,   litnum},rep6a = {NULL,      cmpb,   NULL},
+
+    act7b = {NULL,      sex,    NULL},
+    act7a = {&act7b,    clra,   NULL},  rep7a = {NULL,      clra,   NULL},
+
+    act8b = {NULL,      clra,   NULL},
+    act8a = {&act8b,    cmpd,   litnum},rep8a = {NULL,      cmpb,   NULL},
+
+    act9b = {NULL,      pshs,   d},
+    act9a = {&act9b,    pshs,   x},     rep9a = {NULL,      pshs,   dx},
+
+    act10b = {NULL,     pshs,   d},
+    act10a = {&act10b,  pshs,   y},     rep10a = {NULL,     pshs,   dy},
+
+    act11b = {NULL,     pshs,   u},
+    act11a = {&act11b,  leas,   m2s},   rep11a = {NULL,     pshs,   du},
+
+    act12b = {NULL,     pshs,   u},
+    act12a = {&act12b,  leas,   "-4,s"},rep12a = {NULL,     pshs,   dxu},
+
+    act13b = {NULL,     pshs,   u},
+    act13a = {&act13b,  leas,   "-6,s"},rep13a = {NULL,     pshs,   dxyu};
 
 typedef struct act {
     struct act *nxtact;
@@ -55,6 +82,15 @@ action actions[] = {
     {1,     &act2a,     &rep2a},
     {1,     &act3a,     &rep3a},
     {1,     &act4a,     &rep4a},
+    {1,     &act5a,     &rep5a},
+    {1,     &act6a,     &rep6a},
+    {1,     &act7a,     &rep7a},
+    {1,     &act8a,     &rep8a},
+    {1,     &act9a,     &rep9a},
+    {1,     &act10a,    &rep10a},
+    {1,     &act11a,    &rep11a},
+    {1,     &act12a,    &rep12a},
+    {1,     &act13a,    &rep13a},
     {0,     NULL,       NULL},
 };
 
@@ -77,7 +113,7 @@ action actions[] = {
     { EXIT,     sty,    0,      cmpy,   zero,   1,      1,      0},
     { EXIT,     stu,    0,      cmpu,   zero,   1,      1,      0},
     { EXIT,     ldb,    0,      stb,    1,      1,      1,      0},
-    { REPEAT,   sex,    0,      clra,   0,      2,      0,      0},
+//    { REPEAT,   sex,    0,      clra,   0,      2,      0,      0},
     { EXIT,     leax,   p1x,    0,      m1x,    2,      ",x+",  0},
     { EXIT,     leay,   p1y,    0,      m1y,    2,      ",y+",  0},
     { EXIT,     leau,   p1u,    0,      m1u,    2,      ",u+",  0},
@@ -90,14 +126,14 @@ action actions[] = {
     { EXIT,     leax,   m2x,    0,      zx,     2,      ",--x", 0},
     { EXIT,     leay,   m2y,    0,      zy,     2,      ",--y", 0},
     { EXIT,     leau,   m2u,    0,      zu,     2,      ",--u", 0},
-    { EXIT,     sex,    0,      cmpd,   litnum, cmpb,   2,      0},
-    { EXIT,     clra,   0,      cmpd,   litnum, cmpb,   2,      0},
-    { EXIT,     leas,   p2s,    std,    m2s,    2,      ",s++", 0},
+//    { EXIT,     sex,    0,      cmpd,   litnum, cmpb,   2,      0},
+//    { EXIT,     clra,   0,      cmpd,   litnum, cmpb,   2,      0},
+//    { EXIT,     leas,   p2s,    std,    m2s,    2,      ",s++", 0},
     { EXIT,     leas,   p2s,    pshs,   d,      std,    zs,     0},
     { EXIT,     leas,   p2s,    puls,   upc,    2,      xupc,   0},
-    { EXIT,     pshs,   u,      leas,   m2s,    1,      du,     0},
-    { EXIT,     pshs,   u,      leas,   "-4,s", 1,      dxu,    0},
-    { EXIT,     pshs,   u,      leas,   "-6,s", 1,      dxyu,   0},
+//    { EXIT,     pshs,   u,      leas,   m2s,    1,      du,     0},
+//    { EXIT,     pshs,   u,      leas,   "-4,s", 1,      dxu,    0},
+//    { EXIT,     pshs,   u,      leas,   "-6,s", 1,      dxyu,   0},
     {0},
 };
 */
