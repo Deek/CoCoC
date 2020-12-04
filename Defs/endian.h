0b8b400d7f377f39807c6d2a748df23be41c7cf4
Jeff Teunissen <deek@d2dc.net>
2020-12-04 03:51:20 -0500

    New header: <endian.h>
    Very simple, defines the various types of endianness there can be, and
    declares that C on a 6x09 is big-endian.

---
+++ Defs/endian.h
@@ -0,0 +1,10 @@
+#ifndef	_ENDIAN_H_
+#define	_ENDIAN_H_
+
+#define LITTLE_ENDIAN	1234
+#define BIG_ENDIAN		4321
+#define PDP_ENDIAN		2143
+
+#define BYTE_ORDER		BIG_ENDIAN
+
+#endif	/* _ENDIAN_H_ */
